adca3a2bfcf4c6b8ddbf73cfc0b5287a284f390a
Baptiste <baptiste.rigondaud@ensimag.fr>
2018-04-27 10:53:15 +0200

    almost working skybox, just clean code and inverse top and bottom

--- main.py
+++ main.py
@@ -3,10 +3,15 @@
 import OpenGL.GL as GL
 import glfw
 from viewer import Viewer
+from load import load_textured
 
 def main():
     """ Run the rendering loop for the scene. """
     viewer = Viewer()
+
+    for mesh in load_textured("skybox/skybox.obj"):
+        viewer.add(mesh)
+
     viewer.run()
 
 if __name__ == '__main__':
--- skybox.py
+++ skybox.py
@@ -1,5 +1,6 @@
 from TexturedMesh import TexturedMesh
 from Texture import Texture
+from load import load_textured
 
 class Skybox:
     """
@@ -9,4 +10,3 @@ class Skybox:
         """
         The skybox is made of a cube without depth effect.
         """
-        self.texture = Texture("skyboxTexture/")
